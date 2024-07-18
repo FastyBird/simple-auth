b14e6d25655c66d5a93d4578856b7dad61174d37
Adam Kadlec <adam.kadlec@gmail.com>
2024-07-18 23:33:11 +0200

    Added methods to fetch multiple records via repositories

--- src/Models/Tokens/Repository.php
+++ src/Models/Tokens/Repository.php
@@ -18,12 +18,16 @@ namespace FastyBird\SimpleAuth\Models\Tokens;
 use Doctrine\ORM;
 use Doctrine\Persistence;
 use FastyBird\SimpleAuth\Entities;
+use FastyBird\SimpleAuth\Exceptions;
 use FastyBird\SimpleAuth\Queries;
 use FastyBird\SimpleAuth\Types;
+use IPub\DoctrineOrmQuery;
 use IPub\DoctrineOrmQuery\Exceptions as DoctrineOrmQueryExceptions;
 use Nette;
 use Ramsey\Uuid;
+use Throwable;
 use function assert;
+use function is_array;
 
 /**
  * Security token repository
@@ -111,6 +115,56 @@ final class Repository
 	): Entities\Tokens\Token|null
 	{
 		return $queryObject->fetchOne($this->getRepository($type));
+	}
+
+	/**
+	 * @template T of Entities\Tokens\Token
+	 *
+	 * @param Queries\FindTokens<T> $queryObject
+	 * @param class-string<T> $type
+	 *
+	 * @return array<T>
+	 *
+	 * @throws Exceptions\InvalidState
+	 */
+	public function findAllBy(
+		Queries\FindTokens $queryObject,
+		string $type = Entities\Tokens\Token::class,
+	): array
+	{
+		try {
+			/** @var array<T> $result */
+			$result = $this->getResultSet($queryObject, $type)->toArray();
+
+			return $result;
+		} catch (Throwable $ex) {
+			throw new Exceptions\InvalidState('Fetch all data by query failed', $ex->getCode(), $ex);
+		}
+	}
+
+	/**
+	 * @template T of Entities\Tokens\Token
+	 *
+	 * @param Queries\FindTokens<T> $queryObject
+	 * @param class-string<T> $type
+	 *
+	 * @return DoctrineOrmQuery\ResultSet<T>
+	 *
+	 * @throws DoctrineOrmQueryExceptions\QueryException
+	 * @throws Exceptions\InvalidState
+	 */
+	public function getResultSet(
+		Queries\FindTokens $queryObject,
+		string $type = Entities\Tokens\Token::class,
+	): DoctrineOrmQuery\ResultSet
+	{
+		$result = $queryObject->fetch($this->getRepository($type));
+
+		if (is_array($result)) {
+			throw new Exceptions\InvalidState('Result set could not be created');
+		}
+
+		return $result;
 	}
 
 	/**
